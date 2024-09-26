592f3bced2f1e7e37e475110df00e407a18105ca
Arthur Wambst <wambst.arthur2@gmail.com>
2024-09-26 12:04:53 +0200

    jeu. 26 sept. 2024 12:04:53 CEST

---
+++ test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	int e = 0;
+	a = 1;
+	b = 2;
+	c = 3;
+	d = 4;
+	e = 5;
+    printf("Hello World\n");
+	return 0;
+}
